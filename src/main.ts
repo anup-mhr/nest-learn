4c74c8b654ff7535b0f1a2a2cc540996707cae9c
Anup Maharjan <anupmhrzn16@gmail.com>
2025-06-09 17:00:10 +0545

    feat:add logger and comporession

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+import logger from './utils/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(compression());
+  app.use(cookieParser());
+  app.use(bodyParser.json({ limit: '1mb' }));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   app.enableCors({
     origin: 'http://localhost:3000',
@@ -18,6 +30,14 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3333);
 }
-bootstrap();
+bootstrap().then(() => {
+  logger.success(
+    `| app is running!`,
+    `| env: ${process.env.NODE_ENV}`,
+    `| port: ${3333}`,
+    `| ${new Date().toLocaleString()}`,
+  );
+});
